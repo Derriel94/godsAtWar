97a54353d254c6468c42d4d6dde1cfba971d5cb4
Derriel Collins <derrielcollins96@gmail.com>
2021-04-26 01:36:17 -0500

    Added in styling for gods state, and added some images, and corrected logic issues

--- script.js
+++ script.js
@@ -21,12 +21,23 @@ const Gods = [
 	{ name: 'Anubis',
 	  lore: 'Egyptian',
 	  src: "Images/anubis.png",
-	  desc: 'This is the description of said god'
+	  desc: "Before Osiris took over, Anubis patrolled the underworld." + 
+	    " Anubis was a “psychopomp,” or deity who assists in the afterlife," +
+	    " and was the offspring of Ra and Nephthys. He was known for " +
+	    " mummifying the dead and guiding their souls towards the afterlife. " +
+	    " His skin was black, symbolizing the dark Nile deposits which made the land so fertile. " +
+	    " With the head of a jackal and the body of a man, Anubis also stood for renaissance" +
+	    " and the staining of dead bodies after the embalming process."
 	 },
-	 { name: 'Anubis',
+	 { name: 'Amun-Ra',
 	  lore: 'Egyptian',
-	  src: "Images/anubis.png",
-	  desc: 'This is the description of said god'
+	  src: "Images/amunRa.jpg",
+	  desc: "As Zeus was to the Greeks, the Egyptian god Amun-Ra or Amon was considered the king of the" +
+	  		"gods and goddesses. He became Amun-Ra after being amalgamated with the sun god Ra. He" +
+	  		"was thought to be the father of the pharaohs, and his female counterpart, Amunet, was called the" +
+	  		"Female Hidden One. Forming the Theban Triad, Amun and Mut along with their son Khonsu, the" +
+	  		"moon god, were worshiped throughout ancient Egypt. Amun was not only worshiped in Egypt, but" +
+	  		"idk im so tired bro lol"
 	 },
 	 { name: 'Anubis',
 	  lore: 'Egyptian',
@@ -70,7 +81,7 @@ const Gods = [
 	 },
 
 ];
-console.log(Gods[0].name);
+
 
 // NAVBAR
 
@@ -121,23 +132,29 @@ thanks.onclick = () => {
 // }
 
 const insertGods = () => {
-	title.innerHTML = " <span>&#9876;</span> CHOOSE FROM OUR GODLY DATABASE! <span>&#9876;</span> ";
+	title.innerHTML = " <span>&#9876;</span> Know your Gods! <span>&#9876;</span> ";
 	homeText.remove();
-	const newSection = document.createElement('DIV');
-	newSection.classList.add('section-wrapper');
+	// const newSection = document.createElement('DIV');
+	// newSection.classList.add('section-wrapper');
 	for (i=0; i < Gods.length; i++) {
 	let godDiv = document.createElement('DIV');
 	godDiv.setAttribute("id", "godDiv");
     let godName = document.createElement('P');
+    godName.setAttribute("id", "godName");
     let godImg = document.createElement("IMG");
+    godImg.setAttribute("class", "godImg");
     let godDesc = document.createElement("P");
+    godDesc.setAttribute("id", "godDesc");
+    let godInfo = document.createElement('DIV');
+    godInfo.setAttribute("id", "godInfo");
 	godName.innerHTML = Gods[i].name;
 	godImg.src = Gods[i].src;
 	godDesc.innerHTML = Gods[i].desc;
 	section.appendChild(godDiv);
-	godDiv.appendChild(godName);
 	godDiv.appendChild(godImg);
-	godDiv.appendChild(godDesc);
+	godDiv.appendChild(godInfo);
+	godInfo.appendChild(godName)
+	godInfo.appendChild(godDesc);
     }
 //     isClicked = true;
 //     if (isClicked) {
@@ -177,9 +194,9 @@ const thankYou = () => {
 	section.appendChild(homeText);
 }
 
-const removeit = () => {
-	 homeText.remove();
-}
+// const removeit = () => {
+// 	 homeText.remove();
+// }
 const removeGods = () => {
 	for (i=0; i < Gods.length; i++) {
 		document.getElementById('godDiv').remove();
